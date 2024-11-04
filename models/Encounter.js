84144ea4d001329ea4a2273746d7953f12eab68b
peltimies <ac9194@student.jamk.fi>
2024-11-04 10:37:21 +0200

    ASD

--- models/Encounter.js
+++ models/Encounter.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const EncounterSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  description2: { type: String },
+  roll: { type: String },
   weight: { type: Number, required: true },
   img: { type: String },
 });
